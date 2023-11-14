aa4f01fc50d27c8958d02a24c9baa253308540b7
Kabir <kabirdabholkar@gmail.com>
2023-11-14 11:23:54 +0200

    modified paths.py for local

--- paths.py
+++ paths.py
@@ -1,2 +1,3 @@
 # runs_path = "/snel/share/runs"
-runs_path = "/Users/kabir/Documents/lfads-torch-runs"
+# runs_path = "/Users/kabir/Documents/lfads-torch-runs"
+runs_path = "/home/kabird/lfads-torch-runs"
