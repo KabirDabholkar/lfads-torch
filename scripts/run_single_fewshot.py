43062e7d4b406c960135d30341ed80308b5931a7
Kabir Dabholkar <kabirdabholkar@gmail.com>
2023-11-15 11:29:20 +0100

    restructured configs

--- scripts/run_single_fewshot.py
+++ scripts/run_single_fewshot.py
@@ -8,9 +8,9 @@ from paths import runs_path
 
 # ---------- OPTIONS -----------
 PROJECT_STR = "lfads-torch-example"
-DATASET_STR = "nlb_mc_maze"
-# RUN_TAG = datetime.now().strftime("%y%m%d_%H%M%S") + "_exampleSingle"
-RUN_TAG = "231113_131336_exampleSingle"
+DATASET_STR = "nlb_mc_rtt"
+RUN_TAG = datetime.now().strftime("%y%m%d_%H%M%S") + "_exampleSingle"
+# RUN_TAG = "231113_131336_exampleSingle"
 RUN_DIR = Path(runs_path) / PROJECT_STR / DATASET_STR / RUN_TAG
 OVERWRITE = False
 # ------------------------------
@@ -28,9 +28,9 @@ run_model(
         "datamodule": DATASET_STR,
         "model": DATASET_STR,
     },
-    config_path="../configs/single_few_shot.yaml",
+    config_path="../configs/single_few_shot_mc_rtt.yaml",
     # config_path="../configs/single.yaml",
-    do_train=False,
+    do_train=True,
     do_posterior_sample=False,
     do_fewshot_protocol=False,
 )
